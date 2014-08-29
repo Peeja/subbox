cb3e3b05aa98e2764b2828b6706262b7650d7df4
Peter Jaros <peter.a.jaros@gmail.com>
2014-08-29 15:21:54 -0400

    YouTube API is a value.

--- src/subbox/handler.clj
+++ src/subbox/handler.clj
@@ -1,12 +1,16 @@
 (ns subbox.handler
-  (:require [compojure.core :refer [defroutes GET]]
+  (:require [environ.core :refer [env]]
+            [compojure.core :refer [defroutes GET]]
             [compojure.handler :as handler]
             [compojure.route :as route]
             [hiccup.page :as h]
             [subbox.youtube :as yt]))
 
+(def ^:private yt-api
+  (yt/api (env :google-api-key) "subbox"))
+
 (defn subscriptions [username]
-  (yt/subscriptions (yt/username->cid username)))
+  (yt/subscriptions yt-api (yt/username->cid yt-api username)))
 
 (defn front-page []
   (h/html5
--- src/subbox/youtube.clj
+++ src/subbox/youtube.clj
@@ -1,25 +1,23 @@
-(ns subbox.youtube
-  (:require [environ.core :refer [env]]))
+(ns subbox.youtube)
 
-(def ^:private api-key (env :google-api-key))
-(def ^:private app-name "subbox")
+(defn api [api-key app-name]
+  {:api-key api-key
+   :youtube-api
+   (let [http-transport      (com.google.api.client.http.javanet.NetHttpTransport.)
+         json-factory        (com.google.api.client.json.jackson2.JacksonFactory.)
+         request-initializer (reify
+                               com.google.api.client.http.HttpRequestInitializer
+                               (initialize [_ _] nil))]
+     (-> (com.google.api.services.youtube.YouTube$Builder. http-transport json-factory request-initializer)
+         (.setApplicationName app-name)
+         (.build)))})
 
-(def ^:private youtube
-  (let [http-transport      (com.google.api.client.http.javanet.NetHttpTransport.)
-        json-factory        (com.google.api.client.json.jackson2.JacksonFactory.)
-        request-initializer (reify
-                              com.google.api.client.http.HttpRequestInitializer
-                              (initialize [_ _] nil))]
-    (-> (com.google.api.services.youtube.YouTube$Builder. http-transport json-factory request-initializer)
-        (.setApplicationName app-name)
-        (.build))))
-
-(defn username->cid [username]
+(defn username->cid [api username]
   "Fetches the channel ID for a YouTube username."
-  (-> youtube
+  (-> (:youtube-api api)
       .channels
       (.list "id")
-      (.setKey api-key)
+      (.setKey (:api-key api))
       (.setForUsername username)
       (.setFields "items(id)")
       .execute
@@ -27,12 +25,12 @@
       first
       (get "id")))
 
-(defn subscriptions [cid]
+(defn subscriptions [api cid]
   "Fetches the subscriptions of the given channel (specified by channel ID)."
-  (-> youtube
+  (-> (:youtube-api api)
       .subscriptions
       (.list "snippet")
-      (.setKey api-key)
+      (.setKey (:api-key api))
       (.setChannelId cid)
       (.setFields "items(snippet/title)")
       .execute
