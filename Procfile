284268cc7f62c6c3fa29a5a83c191d90c4054422
Peter Jaros <peter.a.jaros@gmail.com>
2014-09-22 11:03:52 -0400

    lein new chestnut subbox

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/subbox.jar clojure.main -m subbox.server
---
+++ src/cljs/subbox/core.cljs
@@ -0,0 +1,29 @@
+(ns subbox.core
+  (:require [clojure.browser.repl]
+            [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]
+            [figwheel.client :as fw :include-macros true]))
+
+(def localhost? (-> js/window
+                    (.-location)
+                    (.-host)
+                    (.indexOf "localhost")
+                    (>= 0)))
+
+(defonce app-state (atom {:text "Hello world!"}))
+
+(om/root
+  (fn [app owner]
+    (reify om/IRender
+      (render [_]
+        (dom/h1 nil (:text app)))))
+  app-state
+  {:target (. js/document (getElementById "app"))})
+
+(if localhost?
+  (enable-console-print!))
+
+(if localhost?
+ (fw/watch-and-reload
+  :websocket-url   "ws://localhost:3449/figwheel-ws"
+  :jsload-callback (fn [] (print "reloaded"))))
